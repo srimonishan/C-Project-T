c05cf4b3685372afacbc9f82082416a447cd02fc
NadithDulwan <nadithdulwan11@gmail.com>
2026-06-29 22:44:41 +0530

    Add Order and OrderItem models

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,5 +8,12 @@ namespace LocalArtisanCraftMarket.Models
 {
     internal class Order
     {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string OrderStatus { get; set; }   
+        public string DeliveryAddress { get; set; }
+        public string ContactNumber { get; set; }
     }
 }
--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -8,5 +8,14 @@ namespace LocalArtisanCraftMarket.Models
 {
     internal class OrderItem
     {
+        public int OrderItemID { get; set; }
+        public int OrderID { get; set; }
+        public int CraftItemID { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal { get; set; }
+
+        
+        public string ItemName { get; set; }
     }
 }
