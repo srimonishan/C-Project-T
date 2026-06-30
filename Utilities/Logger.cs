d9149bb48b86b7e766864ebc1ec4e33a8bd2b731
Nadun Sachintha <sachinthanadun519@gmail.com>
2026-06-30 12:45:13 +0530

    Create Logs directory if it doesn't exist

--- Utilities/Logger.cs
+++ Utilities/Logger.cs
@@ -11,4 +11,14 @@ namespace LocalArtisanCraftMarket.Utilities
         {
             try
             {
-              
+                Directory.CreateDirectory("Logs");
+                
+                using (StreamWriter writer = new StreamWriter(logFile, true))
+                {
+                    writer.WriteLine("==================================");
+                    writer.WriteLine("Date: " + DateTime.Now);
+                    writer.WriteLine("Message: " + ex.Message);
+                    writer.WriteLine("Stack Trace:");
+                    writer.WriteLine(ex.StackTrace);
+                    writer.WriteLine();
+                }
